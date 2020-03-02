9d24f564e9c9150e0a41b5edce024d28fe501dca
nickollay <nickollaysokolov@gmail.com>
2020-03-03 01:19:06 +0300

    add perfect weight

---
+++ perfect_weight.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+user_name = gets.chomp
+puts "What is your height?"
+height = gets.chomp.to_i
+perfect_weight = (height-110)*1.15
+
+if perfect_weight <= 0
+	puts "#{user_name}, your weigt is alredy perfect!"
+else 
+	puts "#{user_name}, your perfect weight is #{perfect_weight} kg."
+end
